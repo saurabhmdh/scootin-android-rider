5a175c24ad290840cd4dba03c28a2818ca1baa3a
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-10-28 22:24:25 +0900

    small change on application

---
+++ app/src/main/java/com/scootin/view/activity/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.scootin.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.scootin.databinding.ActivitySplashBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+    }
+}
--- app/src/main/java/com/scootin/view/fragment/home/LoginFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/home/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.scootin.view.fragment.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -10,11 +11,12 @@ import com.scootin.databinding.FragmentLoginBinding
 import com.scootin.network.AppExecutors
 import com.scootin.network.response.PendingOrderItemList
 import com.scootin.util.fragment.autoCleared
+import com.scootin.view.activity.MainActivity
 import com.scootin.view.adapter.orders.PendingOrderDetailsItemAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 @AndroidEntryPoint
-class LoginFragment:Fragment(R.layout.fragment_login) {
+class LoginFragment :Fragment(R.layout.fragment_login) {
     private var binding by autoCleared<FragmentLoginBinding>()
 
     @Inject
@@ -24,11 +26,14 @@ class LoginFragment:Fragment(R.layout.fragment_login) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
        binding.btnLogin.setOnClickListener {
-           findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToDashboardFragment())
+           gotoHomePage()
        }
 
     }
 
-
-
+    private fun gotoHomePage() {
+        startActivity(Intent(requireContext(), MainActivity::class.java))
+        activity?.overridePendingTransition(R.anim.enter, R.anim.exit)
+        activity?.finish()
+    }
 }
