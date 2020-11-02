822c7e69a88b9f5e60e0c22087676544ff4b4a74
DESKTOP-0ARHB49\samridhi <samridhi102kri@gmail.com>
2020-11-02 02:06:10 -0800

    no message

--- app/src/main/java/com/scootin/view/fragment/home/orders/AcceptOrderDetailsFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/home/orders/AcceptOrderDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.scootin.view.fragment.home.orders
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
@@ -10,8 +11,10 @@ import com.scootin.R
 import com.scootin.databinding.FragmentPendingOrderDetailsBinding
 import com.scootin.network.AppExecutors
 import com.scootin.network.api.Status
+import com.scootin.network.manager.AppHeaders
 import com.scootin.util.fragment.autoCleared
 import com.scootin.view.adapter.orders.PendingOrderDetailsItemAdapter
+import com.scootin.viewmodel.home.LoginViewModel
 import com.scootin.viewmodel.order.OrdersViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -21,6 +24,10 @@ class AcceptOrderDetailsFragment:Fragment(R.layout.fragment_pending_order_detail
     private var binding by autoCleared<FragmentPendingOrderDetailsBinding>()
 
     private val viewModel: OrdersViewModel by viewModels()
+
+
+
+
 
     @Inject
     lateinit var appExecutors: AppExecutors
@@ -52,6 +59,8 @@ class AcceptOrderDetailsFragment:Fragment(R.layout.fragment_pending_order_detail
                 }
             }
         }
+
+
 
 
 
