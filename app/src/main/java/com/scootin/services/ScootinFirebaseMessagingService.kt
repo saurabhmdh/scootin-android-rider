ce8253002900d788ba44f4e61a790b074f0ed467
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-11-03 03:27:13 +0900

    added deeplinking and navigations

--- app/src/main/java/com/scootin/services/ScootinFirebaseMessagingService.kt
+++ app/src/main/java/com/scootin/services/ScootinFirebaseMessagingService.kt
@@ -1,45 +1,47 @@
 package com.scootin.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.scootin.R
+import com.scootin.util.constants.IntentConstants.openDirectOrderDetail
+import com.scootin.util.constants.IntentConstants.openOrderDetail
+import com.scootin.view.activity.MainActivity
 import timber.log.Timber
 
 class ScootinFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
 
-        Timber.i("Firebase From: ${remoteMessage.from}")
         if (remoteMessage.data.isNotEmpty()) {
-            Timber.i("Firebase Message data payload: ${remoteMessage.data}")
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                handleNow()
-                sendNotification(remoteMessage.data.toString())
+            val type = remoteMessage.data["type"]
+            Timber.i("Notification type $type")
+            when (type) {
+                "NEW_NORMAL_ORDER_TO_RIDERS" -> {
+                    remoteMessage.data["order_id"]?.let {
+                        addNewOrderNotification(it, true)
+                    }
+                }
+                "NEW_DIRECT_ORDER_TO_RIDERS" -> {
+                    remoteMessage.data["order_id"]?.let {
+                        addNewOrderNotification(it, false)
+                    }
+                }
             }
         }
-
-        // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
-            Timber.i("Message Notification Body: ${it.body}")
-            sendNotification(it.body.toString())
-        }
-
     }
 
     override fun onNewToken(token: String) {
-        Timber.i("Firebase Refreshed token: $token")
+        Timber.i("Refreshed token: $token")
         sendRegistrationToServer(token)
-    }
-
-    private fun scheduleJob() {
-    }
-
-    private fun handleNow() {
-        Timber.i("Short lived task is done.")
     }
 
     private fun sendRegistrationToServer(token: String?) {
@@ -47,37 +49,42 @@ class ScootinFirebaseMessagingService : FirebaseMessagingService() {
         Timber.i("sendRegistrationTokenToServer($token)")
     }
 
-    private fun sendNotification(messageBody: String) {
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(
-//            this, 0 /* Request code */, intent,
-//            PendingIntent.FLAG_ONE_SHOT
-//        )
-//
-//        val channelId = getString(R.string.default_notification_channel_id)
-//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-//            .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//            .setContentTitle(getString(R.string.fcm_message))
-//            .setContentText(messageBody)
-//            .setAutoCancel(true)
-//            .setSound(defaultSoundUri)
-//            .setContentIntent(pendingIntent)
-//
-//        val notificationManager =
-//            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//
-//        // Since android Oreo notification channel is needed.
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val channel = NotificationChannel(
-//                channelId,
-//                "Channel human readable title",
-//                NotificationManager.IMPORTANCE_DEFAULT
-//            )
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//
-//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    private fun addNewOrderNotification(orderId: String, isNormal: Boolean) {
+        val intent = Intent(this, MainActivity::class.java)
+
+        if (isNormal) {
+            intent.data = openOrderDetail(orderId)
+        } else {
+            intent.data = openDirectOrderDetail(orderId)
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_icon)
+            .setContentTitle(getString(R.string.new_order_message))
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 }
