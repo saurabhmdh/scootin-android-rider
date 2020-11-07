b979bcb01ee51aef198f7e87fd192159a05db164
DESKTOP-0ARHB49\samridhi <samridhi102kri@gmail.com>
2020-11-07 17:11:32 +0530

    no message

--- app/src/main/java/com/scootin/viewmodel/home/HomeViewModel.kt
+++ app/src/main/java/com/scootin/viewmodel/home/HomeViewModel.kt
@@ -1,20 +1,40 @@
 package com.scootin.viewmodel.home
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 
 import androidx.lifecycle.viewModelScope
+import com.scootin.database.dao.CacheDao
+import com.scootin.extension.MakeLiveData
+import com.scootin.network.AppExecutors
 import com.scootin.repository.UserRepository
+import com.scootin.util.constants.AppConstants
 import com.scootin.viewmodel.base.ObservableViewModel
 import kotlinx.coroutines.Dispatchers
 
 
 class HomeViewModel @ViewModelInject internal constructor(
-    private val userRepo: UserRepository
+    private val userRepo: UserRepository,
+    private val cacheDao: CacheDao,
+    private val executors: AppExecutors
 ) : ObservableViewModel()  {
 
 
     fun getRiderInfo(riderId: String) = userRepo.getRiderInfo(riderId, viewModelScope.coroutineContext + Dispatchers.IO)
+    private val _doLogout = MutableLiveData<Boolean>()
 
+    fun doLogout() {
+        _doLogout.postValue(true)
+    }
+    val logoutComplete = Transformations.switchMap(_doLogout) { request->
+        if (request) {
+            executors.diskIO().execute {
+                cacheDao.delete(AppConstants.USER_INFO)
+            }
+        }
+        MakeLiveData.create(true)
+    }
 
 
 }
