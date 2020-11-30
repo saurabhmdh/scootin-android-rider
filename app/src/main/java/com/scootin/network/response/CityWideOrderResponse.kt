51c767fe2563b37f99783daf32fd99edf51d8262
DESKTOP-0ARHB49\samridhi <samridhi102kri@gmail.com>
2020-11-30 14:57:43 +0530

    no message

---
+++ app/src/main/java/com/scootin/network/response/CityWideOrderResponse.kt
@@ -0,0 +1,23 @@
+package com.scootin.network.response
+
+
+data class CityWideOrderResponse(
+    val PaymentDetails: PaymentDetails,
+    val deliveryAddressDetails: AddressDetails,
+    val id: Int,
+    val media: Media,
+    val orderDate: String,
+    val orderStatus: String,
+    val pickupAddressDetails: AddressDetails,
+    val serviceArea: ServiceArea,
+    val userInfo: UserInfo
+) {
+    data class ServiceArea(
+        val deleted: Boolean,
+        val id: Int,
+        val latitude: Double,
+        val longitude: Double,
+        val name: String,
+        val serviceRadius: Int
+    )
+}
