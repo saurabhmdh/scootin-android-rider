b4ca8ac2b9da43a6405cb2d192ff5aee24043690
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-12-09 17:28:45 +0900

    added code for citywide accept order..

--- app/src/main/java/com/scootin/network/response/OrderListResponse.kt
+++ app/src/main/java/com/scootin/network/response/OrderListResponse.kt
@@ -9,5 +9,6 @@ data class OrderListResponse(
     val orderStatus: String,
     val rejectReason: String,
     val totalAmount: Double,
-    val userInfo: UserInfo
+    val userInfo: UserInfo,
+    val orderType: String
 )
