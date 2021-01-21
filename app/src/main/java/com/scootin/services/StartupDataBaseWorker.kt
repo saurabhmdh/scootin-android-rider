1c56d3e5836670126cb1fc9f6d654984489d70f4
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-01-21 18:09:40 +0900

    updated the FCM ID on every launch

--- app/src/main/java/com/scootin/services/StartupDataBaseWorker.kt
+++ app/src/main/java/com/scootin/services/StartupDataBaseWorker.kt
@@ -3,17 +3,21 @@ package com.scootin.services
 import android.app.Application
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.scootin.database.dao.CacheDao
 import com.scootin.di.IWorkerFactory
 import com.scootin.network.AppExecutors
+import com.scootin.util.constants.AppConstants
 import kotlinx.coroutines.coroutineScope
 import timber.log.Timber
 import javax.inject.Inject
 
 class StartupDataBaseWorker (
     private val context: Application,
-    params: WorkerParameters
+    params: WorkerParameters,
+    val cacheDao: CacheDao
 ): CoroutineWorker(context, params) {
     override suspend fun doWork(): Result = coroutineScope {
+        cacheDao.deleteCache(AppConstants.FCM_ID)
         Timber.i("Running  ..")
         Result.success()
     }
@@ -22,11 +26,13 @@ class StartupDataBaseWorker (
     /* Factory implementation for Work Manager */
     class Factory @Inject constructor(
         val appExecutors: AppExecutors,
-        val application: Application)
+        val application: Application,
+        val cacheDao: CacheDao
+    )
         : IWorkerFactory<StartupDataBaseWorker> {
 
         override fun create(params: WorkerParameters): StartupDataBaseWorker {
-            return StartupDataBaseWorker(application, params)
+            return StartupDataBaseWorker(application, params, cacheDao)
         }
     }
 }
